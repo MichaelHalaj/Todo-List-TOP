ae0291059cc3504ec813d19ce7a533b93f724014
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-23 14:31:48 -0500

    Add menu drop down logic and style

--- src/functions.js
+++ src/functions.js
@@ -5,14 +5,20 @@ const removeAllChildren = (node) => {
 };
 const createAccordion = () => {
   const accordion = document.querySelectorAll('.accordion');
+  const menuDropDown = document.querySelector('.menu-dropdown');
+  const menuUp = document.querySelector('.menu-up');
   for (let i = 0; i < accordion.length; i += 1) {
     accordion[i].addEventListener('click', function () {
       this.classList.toggle('active');
       const panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
+        menuDropDown.classList.remove('no-display');
+        menuUp.classList.add('no-display');
       } else {
         panel.style.maxHeight = `${panel.scrollHeight}px`;
+        menuUp.classList.remove('no-display');
+        menuDropDown.classList.add('no-display');
       }
     });
   }
